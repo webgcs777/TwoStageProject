3cab775a2f75958b7a70e01d4b948a44ad4e5c2c
webgcs777 <73783319+webgcs777@users.noreply.github.com>
2020-11-17 14:32:40 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,125 @@
+$(".header-a-color a").mousemove(function() {
+	//找到a的爹的孩子的下一个元素  
+	$(this).parent().children().next().show();
+	// console.log("456")
+});
+
+$(".header-a-color ul").mousemove(function() {
+	//放到要隐藏的那个元素  身上时  显示
+	$(this).show();
+});
+
+$(".header-a-color ul").mouseout(function() {
+	//移出要隐藏的那个元素  身上时  隐藏
+	$(this).hide();
+});
+
+$(".header-a-color").mouseout(function() {
+	//移出要隐藏的那个元素  身上时  隐藏
+    $(".header-a-color ul").hide();
+});
+
+// 文字变色
+$(".header-a-color a").mouseover(function() {
+	$(this).css({
+		color: "#666666"
+	});
+	$(".yanse").css({
+		color: "#666666"
+	});
+});
+
+$(".header-a-color a").mouseout(function() {
+	$(this).css({
+		color: "#b2b2b2"
+	});
+	$(".yanse").css({
+		color: "#666666"
+	});
+});
+
+
+
+
+
+
+
+// 尾部文字变色
+$(".footer-ul a").mouseover(function() {
+	$(this).css({
+		color: "#666666"
+	});
+
+
+});
+$(".footer-ul a").mouseout(function() {
+	$(this).css({
+		color: "#b2b2b2"
+	});
+
+});
+
+
+
+
+
+
+/* 吸顶 */
+$(function() {
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 180) {
+			$(".xiding").css({
+				"display":"block",
+				"position": "fixed",
+				"top": 0,		
+				"width": "100%",
+				"z-index": 55
+			});
+		} else {
+			$(".xiding").css({
+				"position": "static",
+				"display":"none"
+			});
+		}
+	});
+});
+
+//子页面点击购物车跳转到购物车
+
+//加一减一
+// $(".button-1").click(function(){
+// 	$(this)
+// });
+//跳转到首页
+$(".Dshouye").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/index.html"
+	})
+//跳转到登陆
+	$(".login").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/index.html"
+	})
+	//跳转到注册
+	$(".registered").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/registered.html"
+		
+	})
+	//调到 关于嘉木
+	$(".guanyu").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/About.html"
+		
+	})
+	//调用  联系我们
+	$(".lianxi").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/League.html"
+		
+	})
+    //调用   加盟方式
+    $(".jiameng").click(()=>{
+		location.href="http://127.0.0.1/2008/Project/Contact.html"
+		
+	})
+	//调用   节气故事
+	$(".yan").click(()=>{
+			location.href="http://127.0.0.1/2008/Project/SolarTerms.html"
+			
+		})
---
+++ lunbotu.js
@@ -0,0 +1,87 @@
+class Banner {
+	//判断一个类是否需要参数
+	//从HTML是否需要获取元素
+	constructor(newImg, newLis) {
+		//初始的index为0
+		this.index = 0;
+		this.oImg = newImg;
+		this.oLis = newLis;
+		//设置初始的背景图
+		this.oImg.style.backgroundImage = "url(img/" + this.index + ".jpg)";
+		//设置初始红色点的位置
+		//oLis数组
+		this.oLis[this.index].style.backgroundColor = "red";
+	}
+
+	//设置背景
+	setBgcImage() {
+		this.oImg.style.backgroundImage = "url(img/" + this.index + ".jpg)";
+	}
+
+	//设置li颜色
+	setLiColor() {
+
+		for (let i = 0; i < this.oLis.length; i++) {
+			if (this.index == i) {
+				this.oLis[i].style.backgroundColor = "red";
+			} else {
+				this.oLis[i].style.backgroundColor = "#ffffff";
+			}
+		}
+	}
+	//点击先后一步
+	next() {
+		this.index++;
+		//如果index（图的位置）等于oLis.length（li的总数量也可以理解为最大值）
+		if (this.index == this.oLis.length) {
+			//那么就让index（图的位置）等于0  ，等于0就是等于第一张
+			this.index = 0;
+		}
+		//再次调用函数
+		this.setBgcImage();
+		this.setLiColor();
+
+	}
+	//点击向前一步
+	prev() {
+		this.index--;
+		if (this.index == -1) {
+			this.index = this.oLis.length - 1;
+		}
+		this.setBgcImage();
+		this.setLiColor();
+	}
+	//小黑点
+	click() {
+		let that = this;
+		for (let i = 0; i < this.oLis.length; i++) {
+			this.oLis[i].onclick = function() {
+				that.index = i;
+				that.setBgcImage();
+				that.setLiColor();
+			}
+		}
+	}
+	//事件
+	getEvent() {
+		let oBtnNext = document.getElementById("btn-next");
+		let oBtnPrev = document.getElementById("btn-previous");
+		let that = this;
+		oBtnNext.onclick = function() {
+			that.next();			
+		}
+    //自动播放
+    
+	setInterval(()=>{
+			that.next() 
+		},5000)
+	 
+		oBtnPrev.onclick = function() {
+			that.prev();
+		}
+		this.click();
+	}
+
+
+}
+
